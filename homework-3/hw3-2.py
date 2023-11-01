346a4845e64677d166a2d79df36f53c663cb6e11
Andrew Meshkov <meshkov89@gmail.com>
2023-11-01 22:19:13 +0300

    feat: add hw4-1.py

--- homework-3/hw3-2.py
+++ homework-3/hw3-2.py
@@ -6,3 +6,12 @@
 1 * 3 = 3
 ...
 """
+
+# Определим количество цифр
+numbers = range(1, 10)
+
+# Цикл вывода таблицы умножения
+for number_a in numbers:
+    for number_b in numbers:
+        result = number_a * number_b
+        print(f'{number_a} * {number_b} = {result}')
