bdd582f361264f1f70b6698d7ea39523dce45e05
Andrew Meshkov <meshkov89@gmail.com>
2023-11-18 21:47:01 +0300

    feat: finish count voina i mir

--- find-war-in-war-and-peace/main.py
+++ find-war-in-war-and-peace/main.py
@@ -2,16 +2,24 @@
 with open('./Tolstoj-Lev-Vojna-i-mir-Kniga-1.txt', 'r') as f:
     file = f.readlines()
 
-
 WORD_TO_FIND = 'война'  # искомое слово
 counter = 0             # счетчик
+symbols_to_remove = ',.!?'
 
 # ищем совпадения искомого слова
 for i in range(len(file)):
+    # разбиваем файл на списки
     arr = file[i].split(' ')
 
     for word in range(len(arr)):
-        if WORD_TO_FIND.lower() == arr[word].lower():
+        # берем конкретное слово из списка и приводим его к нижнему регистру
+        concurrent_word = arr[word].lower()
+        # далее нужно очистить строку от спецсимволов
+        for symbol in symbols_to_remove:
+            concurrent_word = concurrent_word.replace(symbol, '')
+        # сравниваем искомое значение со строкой которую получили
+        if WORD_TO_FIND.lower() == concurrent_word:
+            # в случае успеха увеличиваем счетчик
             counter += 1
 
 # выводим сообщение в консоль с результатом
